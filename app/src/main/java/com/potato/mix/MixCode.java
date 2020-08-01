dbbc9cb6722a0169618447789cf8219fb59803a3
王国立 <guoliwang@epochcredit.com>
2020-08-01 23:22:29 +0800

    修改注解类

--- app/src/main/java/com/potato/mix/MixCode.java
+++ app/src/main/java/com/potato/mix/MixCode.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * create by Potato
+ * create time
+ * Description：找到带有@MixCode的方法（目的：找到插桩的代码内容，进行插桩）
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MixCode {
--- app/src/main/java/com/potato/mix/MixExclude.java
+++ app/src/main/java/com/potato/mix/MixExclude.java
@@ -4,14 +4,13 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
 /**
  * create by Potato
  * create time
- * Description：找到带有@MixClass的类（目的：找到它并且找出需要插桩的代码）
+ * Description：排除带有@MixExclude的类
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface MixClass {
+public @interface MixExclude {
     String value() default "";//备用
 }
