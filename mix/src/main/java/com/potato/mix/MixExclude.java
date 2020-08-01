8787b4e59ea64acd4690237424d2cbb7f79f4d54
王国立 <guoliwang@epochcredit.com>
2020-08-01 23:33:32 +0800

    修改注解类

---
+++ mix/src/main/java/com/potato/mix/MixExclude.java
@@ -0,0 +1,12 @@
+package com.potato.mix;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MixExclude {
+    String value() default "";//备用
+}
