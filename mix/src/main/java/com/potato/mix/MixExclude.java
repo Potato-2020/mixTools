79092a69ef91a6bb50b613ba8936fd03be99116d
王国立 <guoliwang@epochcredit.com>
2020-08-06 19:02:14 +0800

    修改注解类

--- mix/src/main/java/com/potato/mix/MixExclude.java
+++ mix/src/main/java/com/potato/mix/MixExclude.java
@@ -4,7 +4,11 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+/**
+ * create by Potato
+ * create time
+ * Description：添加注解后，插桩失效
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MixExclude {
--- mix/src/main/java/com/potato/mix/MixTemplate.java
+++ mix/src/main/java/com/potato/mix/MixTemplate.java
@@ -4,7 +4,11 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+/**
+ * create by Potato
+ * create time
+ * Description：插桩模板类，插桩内容从模板获取
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MixClass {
